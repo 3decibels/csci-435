8ac40cbc0c7484c7d6a2ccfba0c4c4142c9415bf
Adam Sebzda <asebzda@gmail.com>
2019-09-09 23:36:56 -0400

    Completed assignment 01

--- assignment01/assignment01.c
+++ assignment01/assignment01.c
@@ -9,16 +9,28 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	char buf[32] = "";
-	printf("Input text: ");
-	scanf("%32s", buf);
+    char buf[32] = "";
+    int charInt = 0;
+    int charOffset = 97;
 
-	for (int i = 48; i <= 122; i++) {
-		for (int c = 0; c < strlen(buf); c++) {
-			if (i == (int)buf[c]) {
-				printf("%c\n", buf[c]);
-				break;
-			}
-		}
-	}
+    printf("Input text: ");
+    scanf("%32s", buf);
+
+    // Search for ASCII chars in the buffer
+    for (int c = 0; c <= strlen(buf); c++) {
+        for (int i = 0; i <= 25; i++) {
+            if ((int)buf[c] == i + charOffset) {
+                // Store discovered chars into charInt
+                charInt = charInt | (0x01 << i);
+                break;
+            }
+        }
+    }
+
+    // Write out the chars stored into charInt
+    for (int i = 0; i <= 25; i++) {
+        if (charInt & (0x01 << i)) {
+            printf("%c\n", i + charOffset);
+        }
+    }
 }
