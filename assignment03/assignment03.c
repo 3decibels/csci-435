1257b3d64f365c07d48e756f4ffc1ab8d88a092c
Adam Sebzda <asebzda@gmail.com>
2019-10-01 01:25:15 +0000

    Cleanup assignment03

--- assignment03/assignment03.c
+++ assignment03/assignment03.c
@@ -16,12 +16,13 @@
 
 
 int main(int argc, char *argv[]) {
-    int fd[2], fsync[2];
+    int dn, fd[2], fsync[2];
     pid_t pid, termPid;
     char buf[32], letter;
 
     printf("Input a string: ");
     scanf("%31s", buf);
+
 
     if (pipe(fd) == -1) {
         fprintf(stderr, "Pipe failed: %s\n", strerror(errno));
@@ -42,29 +43,36 @@ int main(int argc, char *argv[]) {
         // Prints and feeds data to child PID
         int ret;
         char nothing;
+
         close(fd[0]);
         close(fsync[1]);
+
         for (int i = strlen(buf)-1; i >= 0; i = i-2) {
             printf("parent process (PID: %ld) prints %c\n", (long)getpid(), buf[i]);
             if (i-1 >= 0) {
                 write(fd[1], &buf[i-1], 1);
+                // Is there an acceptable way to read this into nowhere?
+                // How can I block on this pipe and throw the data away without putting it into memory?
                 read(fsync[0], &nothing, 1);
             }
         }
+
         close(fd[1]);
         close(fsync[0]);
+
         termPid = wait(&ret);
         printf("child process with PID %ld terminates\n", termPid);
     }
     else if (pid == 0) {
         // Child PID reads data from pipe
-        char n = 'n';
         close(fd[1]);
         close(fsync[0]);
+
         while(read(fd[0], &letter, 1) > 0) {
             printf("child process (PID: %ld) prints %c\n", (long)getpid(), letter);
             write(fsync[1], &letter, 1);
         }
+
         close(fd[0]);
         close(fsync[1]);
     }
