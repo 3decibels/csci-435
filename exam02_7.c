7598469852c5e857b7383b7483d9767159062cfa
Adam Sebzda <asebzda@gmail.com>
2019-10-16 19:41:31 -0400

    Added exam 2 code

---
+++ exam02_7.c
@@ -0,0 +1,49 @@
+/******************
+ * exam02_7.c
+ * Author: Adam Sebzda
+ * Class:  CSCI-435
+ *****************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+    int dn, fd[2];
+    pid_t pid, termPid;
+
+    if (pipe(fd) == -1) {
+        fprintf(stderr, "Pipe failed: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    pid = fork();
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+    else if (pid > 0) {
+        close(fd[0]);
+        printf("Parent process goes first\n");
+        write(fd[1], 0, 1);
+        close(fd[1]);
+        wait();
+    }
+    else if (pid == 0) {
+        // Child PID reads data from pipe
+        int nothing;
+        close(fd[1]);
+        read(fd[0], &nothing, 1);
+        printf("Child process goes second\n");
+        close(fd[0]);
+    }
+
+    return EXIT_SUCCESS;
+}
